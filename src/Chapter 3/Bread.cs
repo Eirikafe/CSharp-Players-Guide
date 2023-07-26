a76794291e56880c0d8aa266b6ec3f9e9cc94863
Eiriakfe <eirikafe@hotmail.com>
2023-07-26 18:26:59 +0800

    Fixed structure and added previous code

---
+++ src/Chapter 3/Bread.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+var user = "";
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+user = Console.ReadLine();
+Console.WriteLine("Noted: " + user + " got bread.");
---
+++ src/Chapter 4/ObjectOfDoom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.WriteLine("What kind of thing are we talking about?");
+//The object in the sentence
+string a = Console.ReadLine();
+//Descriptors of the object
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();
+//adding random mumbo jumbo
+string c = "of Doom";
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
+
